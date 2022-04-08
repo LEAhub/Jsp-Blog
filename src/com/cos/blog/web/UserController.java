eeb96d498c7944fb3ded35e1169deb1b2fb1bfc2
LEA <emm9811@gmail.com>
2022-04-08 18:27:48 +0900

    add

---
+++ src/com/cos/blog/web/UserController.java
@@ -0,0 +1,56 @@
+package com.cos.blog.web;
+
+import com.cos.blog.domain.user.dto.JoinReqDto;
+import com.cos.blog.domain.user.dto.LoginReqDto;
+import com.cos.blog.service.UserService;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPrecess(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPrecess(request, response);
+    }
+
+    //http://localhost:8080/blog/user?cmd=
+    protected void doPrecess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String cmd = request.getParameter("cmd");
+        UserService service = new UserService();
+        //http://localhost:8080/blog/user?cmd=loginForm
+        if(cmd.equals("loginForm")){
+            response.sendRedirect("user/loginForm.jsp");
+        }else if(cmd.equals("login")){
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            LoginReqDto dto = new LoginReqDto();
+            dto.setUsername(username);
+            dto.setPassword(password);
+
+            service.로그인(dto);
+        }else if(cmd.equals("joinForm")){
+            response.sendRedirect("user/joinForm.jsp");
+        }else if(cmd.equals("join")){
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            String email = request.getParameter("email");
+            String address = request.getParameter("address");
+            JoinReqDto dto = new JoinReqDto();
+            dto.setUsername(username);
+            dto.setPassword(password);
+            dto.setEmail(email);
+            dto.setAddress(address);
+
+            service.회원가입(dto);
+        }
+    }
+}
