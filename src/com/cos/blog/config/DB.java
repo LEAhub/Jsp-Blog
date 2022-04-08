bfe3f527a0b525d60f56c87003325c621bde82e6
LEA <emm9811@gmail.com>
2022-04-09 03:01:02 +0900

    closeDB() overload

--- src/com/cos/blog/config/DB.java
+++ src/com/cos/blog/config/DB.java
@@ -5,6 +5,7 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DB {
     public static Connection getConnection() {
@@ -24,6 +25,13 @@ public class DB {
     public static void closeDB(Connection conn, PreparedStatement pstmt){
         if(conn != null)try{conn.close();}catch (Exception e){}
         if(pstmt != null)try{pstmt.close();}catch (Exception e){}
+
+    }
+
+    public static void closeDB(Connection conn, PreparedStatement pstmt, ResultSet rs){
+        if(conn != null)try{conn.close();}catch (Exception e){}
+        if(pstmt != null)try{pstmt.close();}catch (Exception e){}
+        if(rs != null)try{rs.close();}catch (Exception e){}
 
     }
 
