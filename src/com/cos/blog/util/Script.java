24701974781950959a15a3c70d94660b57bb9db4
LEA <emm9811@gmail.com>
2022-04-08 21:57:15 +0900

    Edit Script.java

--- src/com/cos/blog/util/Script.java
+++ src/com/cos/blog/util/Script.java
@@ -1,4 +1,19 @@
 package com.cos.blog.util;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Script {
+
+    public static void back(HttpServletResponse response, String msg)
+                throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<scrpit>");
+        out.println("alret("+msg+")");
+        out.println("history.back();");
+        out.println("</scrpit>");
+        out.flush(); //buffer 비우기
+        
+    }
 }
